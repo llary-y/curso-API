b763dbb7687a1df04d6bb52740110a61c0936670
llary-y <laryssa02@gmail.com>
2023-10-24 18:57:06 -0300

    interface user repository

---
+++ src/main/java/br/com/praticando/api/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.praticando.api.repository;
+
+import br.com.praticando.api.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
