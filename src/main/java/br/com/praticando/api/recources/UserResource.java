0ce6a2b168f178d6be5676316481fb2ab3cc6c78
llary-y <laryssa02@gmail.com>
2023-10-24 18:59:15 -0300

    classe resource findById

---
+++ src/main/java/br/com/praticando/api/recources/UserResource.java
@@ -0,0 +1,26 @@
+package br.com.praticando.api.recources;
+
+import br.com.praticando.api.domain.User;
+import br.com.praticando.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+
+    @Qualifier("userService")
+    @Autowired
+    private UserService service;
+
+    @GetMapping(value = "/{id}")
+    public  ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+}
